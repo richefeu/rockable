abd99134a707c76feea29e089cbf78c3889f928b
Vincent Richefeu <vincent.richefeu@3sr-grenoble.fr>
2019-02-08 17:28:47 +0100

    add GPL text at beginning of each source file

--- src/BodyForce_PreferredDirection.hpp
+++ src/BodyForce_PreferredDirection.hpp
@@ -1,3 +1,19 @@
+//        Rockable, 3D-DEM with sphero-polyhedra
+//        Copyright (C) 2016-2019  <vincent.richefeu@3sr-grenoble.fr>
+//        
+//        This program is free software: you can redistribute it and/or modify
+//        it under the terms of the GNU General Public License as published by
+//        the Free Software Foundation, either version 3 of the License, or
+//        (at your option) any later version.
+//        
+//        This program is distributed in the hope that it will be useful,
+//        but WITHOUT ANY WARRANTY; without even the implied warranty of
+//        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//        GNU General Public License for more details.
+//        
+//        You should have received a copy of the GNU General Public License
+//        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef BODYFORCE_PREFERREDDIRECTION_HPP_021F7DFB
 #define BODYFORCE_PREFERREDDIRECTION_HPP_021F7DFB
 
--- src/DrivingSystem.hpp
+++ src/DrivingSystem.hpp
@@ -1,15 +1,18 @@
-// Copyright (C) Rockable <vincent.richefeu@3sr-grenoble.fr>
-//
-// This file is part of mbox.
-//
-// Rockable can not be copied and/or distributed without the express
-// permission of the authors.
-// It is coded for academic purposes.
-//
-// Note
-// Without a license, the code is copyrighted by default.
-// People can read the code, but they have no legal right to use it.
-// To use the code, you must contact the author directly and ask permission.
+//        Rockable, 3D-DEM with sphero-polyhedra
+//        Copyright (C) 2016-2019  <vincent.richefeu@3sr-grenoble.fr>
+//        
+//        This program is free software: you can redistribute it and/or modify
+//        it under the terms of the GNU General Public License as published by
+//        the Free Software Foundation, either version 3 of the License, or
+//        (at your option) any later version.
+//        
+//        This program is distributed in the hope that it will be useful,
+//        but WITHOUT ANY WARRANTY; without even the implied warranty of
+//        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//        GNU General Public License for more details.
+//        
+//        You should have received a copy of the GNU General Public License
+//        along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef DRIVING_SYSTEM_HPP
 #define DRIVING_SYSTEM_HPP
