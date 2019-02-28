9d0735da18e7aedd7dd3d74fdce959dc7f1648d3
Vincent Richefeu <vincent.richefeu@3sr-grenoble.fr>
2019-02-28 12:09:53 +0100

    remove unused files

--- common/map1d.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MAP1D_HPP_1C0912F2
-#define MAP1D_HPP_1C0912F2
-
-template <typename T>
-T map1d(T value, T istart, T istop, T ostart, T ostop) {
-  return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
-}
-
-#endif /* end of include guard: MAP1D_HPP_1C0912F2 */
--- common/typedefs.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef TYPEDEFS_HPP_CB5D6DE2
-#define TYPEDEFS_HPP_CB5D6DE2
-
-typedef double real;
-typedef unsigned int uint;
-
-#endif /* end of include guard: TYPEDEFS_HPP_CB5D6DE2 */
