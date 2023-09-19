e30a4e2c85376f509cde1036a6630a49f934ec9e
Vincent Richefeu <33054369+richefeu@users.noreply.github.com>
2023-09-19 10:04:28 +0200

    Update conf.py

--- sphinxdoc/source/conf.py
+++ sphinxdoc/source/conf.py
@@ -33,6 +33,7 @@ release = '1.0.0'
 extensions = [
     'sphinx.ext.mathjax',
     'sphinx_rtd_theme',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
